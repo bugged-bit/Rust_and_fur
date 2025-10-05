a37bfcd7149cba088ce6557bd429ebd81bc0d011
pryansh <pryansh2020@gmail.com>
2025-10-05 15:44:45 +0530

    gg

--- scripts/robot.gd
+++ scripts/robot.gd
@@ -1,22 +1,37 @@
 extends Node2D
 
+# ------------------------
+# EXPORTS
+# ------------------------
 @export var batteries_needed: int = 5
 @export var dialogue_path_first: String = "res://Dialogues/robot_intro.dialogue"
 @export var dialogue_path_second: String = "res://Dialogues/robot_question.dialogue"
 
+# ------------------------
+# NODE REFERENCES
+# ------------------------
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var interaction_area: Area2D = $Area2D
 @onready var timer3: Timer = $Timer3
 
+# ------------------------
+# VARIABLES
+# ------------------------
 var battery_count: int = 0
 var revived: bool = false
 var player_in_range: bool = false
 
+# ------------------------
+# READY
+# ------------------------
 func _ready() -> void:
-	# Connect signals for detecting player
+	# Connect signals for player proximity
 	interaction_area.connect("body_entered", Callable(self, "_on_area_entered"))
 	interaction_area.connect("body_exited", Callable(self, "_on_area_exited"))
 
+# ------------------------
+# AREA SIGNALS
+# ------------------------
 func _on_area_entered(body):
 	if body.is_in_group("player"):
 		player_in_range = true
@@ -25,33 +40,37 @@ func _on_area_exited(body):
 	if body.is_in_group("player"):
 		player_in_range = false
 
+# ------------------------
+# PROCESS INPUT
+# ------------------------
 func _process(_delta):
 	if player_in_range and Input.is_action_just_pressed("inspect"):
-		_start_dialogue()
+		start_dialogue()
 
-# --------------------
-# BATTERY SYSTEM
-# --------------------
+# ------------------------
+# BATTERY LOGIC
+# ------------------------
 func add_battery() -> void:
 	if revived:
 		return
-
+	
 	battery_count += 1
 	Global.battery_count += 1
 	print("+1 battery collected! Total: %d/%d" % [battery_count, batteries_needed])
 
 	if battery_count >= batteries_needed:
-		_revive()
+		revive()
 
-func _revive() -> void:
+func revive() -> void:
 	revived = true
 	sprite.play("idle")
 	print("Robot revived!")
+	# Optional: you can trigger Timer3 or visual effects here
 
-# --------------------
-# DIALOGUE SYSTEM
-# --------------------
-func _start_dialogue():
+# ------------------------
+# DIALOGUE LOGIC
+# ------------------------
+func start_dialogue():
 	var dialogue_resource = null
 
 	if Global.robot_met_before:
@@ -68,11 +87,3 @@ func _start_dialogue():
 	else:
 		DialogueManager.show_dialogue(dialogue_resource)
 		Global.robot_met_before = true
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
